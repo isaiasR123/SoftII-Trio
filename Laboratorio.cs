0836a8f61c781503517108a7864707051510c43d
isaiasR123 <isaias.ramirezet12d1@gmail.com>
2026-05-08 09:32:57 -0300

    Add constructor to Laboratorio class for validation

--- Laboratorio.cs
+++ Laboratorio.cs
@@ -5,4 +5,9 @@ class Laboratorio
     private string nombre { get; set; }
     private string ubicacion { get; set; }
 
+    public Laboratorio(string nombre, string ubicacion){
+      ValidarLaboratorio.ValidarNombre(nombre);
+        ValidarLaboratorio.ValidarUbicacion(ubicacion);
+    }
+
 }
