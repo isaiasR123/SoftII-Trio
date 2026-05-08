17c4467c3e8a020d179f1d59bd8bc565236d252a
isaiasR123 <isaias.ramirezet12d1@gmail.com>
2026-05-08 11:14:48 -0300

    Update sfs.sql

--- scripts bd/sfs.sql
+++ scripts bd/sfs.sql
@@ -1,61 +1,58 @@
-USE 5to_Monitoreo;
-
 DELIMITER $$
+    
+DROP FUNCTION IF EXISTS CalcularEstado $$
 
-
-DROP FUNCTION IF EXISTS fn_calcular_estado$$
-
-CREATE FUNCTION fn_calcular_estado(
-    p_temperatura DECIMAL(5,2),
-    p_conectada BOOLEAN
+CREATE FUNCTION CalcularEstado(
+    untemperatura DECIMAL(5,2),
+    unconectada BOOLEAN
 )
 RETURNS VARCHAR(20)
 READS SQL DATA
 DETERMINISTIC
 BEGIN
-    DECLARE v_estado VARCHAR(20);
+    DECLARE estado VARCHAR(20);
     
-    IF p_conectada = FALSE THEN
-        SET v_estado = 'DESCONECTADA';
-    ELSEIF p_temperatura > 80 THEN
-        SET v_estado = 'CRÍTICA';
-    ELSEIF p_temperatura > 60 THEN
-        SET v_estado = 'ALERTA';
-    ELSEIF p_temperatura > 40 THEN
-        SET v_estado = 'NORMAL';
+    IF conectada = FALSE THEN
+        SET estado = 'DESCONECTADA';
+    ELSEIF temperatura > 80 THEN
+        SET estado = 'CRÍTICA';
+    ELSEIF temperatura > 60 THEN
+        SET estado = 'ALERTA';
+    ELSEIF temperatura > 40 THEN
+        SET estado = 'NORMAL';
     ELSE
-        SET v_estado = 'BAJA';
+        SET estado = 'BAJA';
     END IF;
     
-    RETURN v_estado;
+    RETURN estado;
 END$$
 
 
-DROP FUNCTION IF EXISTS fn_promedio_temperatura$$
+DROP FUNCTION IF EXISTS PromedioTemperatura $$
 
-CREATE FUNCTION fn_promedio_temperatura(
-    p_idComputadora INT,
-    p_fechaInicio DATETIME,
-    p_fechaFin DATETIME
+CREATE FUNCTION PromedioTemperatura(
+    unidComputadora INT,
+    unfechaInicio DATETIME,
+    unfechaFin DATETIME
 )
 RETURNS DECIMAL(5,2)
 READS SQL DATA
 DETERMINISTIC
 BEGIN
-    DECLARE v_promedio DECIMAL(5,2);
+    DECLARE promedio DECIMAL(5,2);
     
     SELECT AVG(temperatura)
-    INTO v_promedio
+    INTO promedio
     FROM Mediciones 
-    WHERE idComputadora = p_idComputadora
-    AND Fechahora BETWEEN p_fechaInicio AND p_fechaFin;
+    WHERE idComputadora = unidComputadora
+    AND Fechahora BETWEEN unfechaInicio AND unfechaFin;
     
   
-    IF v_promedio IS NULL THEN
-        SET v_promedio = 0.00;
+    IF promedio IS NULL THEN
+        SET promedio = 0.00;
     END IF;
     
-    RETURN v_promedio;
+    RETURN promedio;
 END$$
 
 DELIMITER ;
