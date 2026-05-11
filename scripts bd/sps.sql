db6159f06deafd114b3047141c88ffc9955275ca
isaiasR123 <isaias.ramirezet12d1@gmail.com>
2026-05-11 13:50:30 -0300

    Update sps.sql

--- scripts bd/sps.sql
+++ scripts bd/sps.sql
@@ -1,62 +1,52 @@
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS sp_registrar_laboratorio
+DROP PROCEDURE IF EXISTS sp_registrar_laboratorio $$
 
-create procedure sp_registrar_laboratorio( unidLaboratorio int,
-unnombre varchar(45),
-unubicacion varchar(45)
+CREATE PROCEDURE sp_registrar_laboratorio(
+    unidLaboratorio INT,
+    unnombre VARCHAR(45),
+    unubicacion VARCHAR(45)
 )
-
 BEGIN
-  INSERT into Laboratorio(idLaboratorio,nombre,ubicacion)
-      VALUES(unidLaboratorio, unnombre ,unubicacion);
+    INSERT INTO Laboratorio(idLaboratorio, nombre, ubicacion)
+    VALUES(unidLaboratorio, unnombre, unubicacion);
 END $$
 
-DROP PROCEDURE IF EXISTS sp_registrar_modelo_pc
+DROP PROCEDURE IF EXISTS sp_registrar_modelo_pc $$
 
-CREATE PROCEDURE sp_registrar_modelo_pc  (unidModelo int,
-unmarca varchar(45),
-unmodelo varchar(45),
-unprocesador varchar(45),
-unram int)
-
+CREATE PROCEDURE sp_registrar_modelo_pc(
+    unidModelo INT,
+    unmarca VARCHAR(45),
+    unmodelo VARCHAR(45),
+    unprocesador VARCHAR(45),
+    unram INT
+)
 BEGIN
-  INSERT INTO Modelo(idModelo,
-  marca,
-  modelo,
-  procesaodr,
-  ram)
-  VALUES(unidModelo,
-  unmarca,
-  unmodelo,
-  unprocesador,
-  unram);
+    INSERT INTO Modelo(idModelo, marca, modelo, procesador, ram) 
+    VALUES(unidModelo, unmarca, unmodelo, unprocesador, unram);
 END $$
 
-DROP PROCEDURE IF EXISTS sp_registrar_computadora
+DROP PROCEDURE IF EXISTS sp_registrar_computadora $$
 
 CREATE PROCEDURE sp_registrar_computadora(
-unidLaboratorio INT,
-unidModelo INT,
-unidComputadora INT,
-unnombre_equipo VARCHAR(45)
+    unidLaboratorio INT,
+    unidModelo INT,
+    unidComputadora INT,
+    unnombre_equipo VARCHAR(45)
 )
-
 BEGIN
-  INSERT INTO Computadora(
-  idLaboratorio,
-  idModelo,
-  idComputadora,
-  nombre_equipo)
-  VALUES(
-  unidLaboratorio,
-  unidModelo,
-  unidComputadora,
-  unnombre_equipo
-  )
+    INSERT INTO Computadora(
+        idLaboratorio,
+        idModelo,
+        idComputadora,
+        nombre_equipo
+    )
+    VALUES(
+        unidLaboratorio,
+        unidModelo,
+        unidComputadora,
+        unnombre_equipo
+    );  
 END $$
 
-
-
-DELIMITER $$
-
+DELIMITER ;  
