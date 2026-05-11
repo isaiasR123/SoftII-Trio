450b6678eefc0995f80f1b603a01ae17f160dc62
isaiasR123 <isaias.ramirezet12d1@gmail.com>
2026-05-11 11:46:42 -0300

    Add validation methods for PC model attributes

---
+++ ValidarModeloPc.cs
@@ -0,0 +1,53 @@
+namespace proyecto;
+
+public static class ValidarModeloPc
+{
+    ValidarMarca(string marca){
+       if(string.IsNullOrWhiteSpace(marca)){
+         throw new Exception("La marca no puede ser vacia");
+       }
+    }
+
+    ValidarModelo(string modelo){
+        if(string.IsNullOrWhiteSpace(modelo)){
+            throw new Exception("El modelo no puede estar vacio");
+        }
+    }
+
+    ValidarProcesador(int procesador){
+        if(procesador < 0 || procesador > 100 ){
+            throw new Exception("Procesador tiene que ser mayor que 0 y menor que 100");
+        }
+    }
+
+    ValidarRam(int ram){
+        if(ram < 0 || ram > 32){
+            throw new Exception("Ram debe ser mayor que 0 y menor que 32");
+        }
+    }
+
+    ValidarTemperatura(int temperatura){
+        if(temperatura < 0 || temperatura > 50){
+            throw new Exception("Temperatura debe ser mayor que 0 y menor que 50");
+        }
+    }
+
+
+    ValidarConectada(bool conectada){
+        if(!conectada){
+            throw new Exception("Error: debe estar conectado");
+        }
+    }
+    
+    ValidarFechaHora(DateTime fechahora){
+         if (fechahora == DateTime.MinValue)
+    {
+        throw new Exception("La fecha y hora no pueden estar vacías.");
+    }
+
+    if (fechahora > DateTime.Now)
+    {
+        throw new Exception("La fecha no puede ser futura.");
+    }
+    }
+    }
